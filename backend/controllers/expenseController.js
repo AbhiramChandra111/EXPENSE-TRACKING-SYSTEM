14d48ddf0fbcef311eb33e719cfe079d7b24b2bd
KATTA ABHIRAM CHANDRA <164200901+AbhiramChandra111@users.noreply.github.com>
2025-08-24 23:16:14 +0530

    Add files via upload

---
+++ backend/controllers/expenseController.js
@@ -0,0 +1,52 @@
+const User = require("../models/User");
+const Expense = require("../models/Expense");
+
+// Add Expense Source
+exports.addExpense = async (req, res) => {
+    const userId = req.user.id;
+    try{
+        const { icon, category, amount, date } = req.body;
+
+        if(!category || !amount || !date){
+            return res.status(400).json({ message: "All fields are required" });
+        }
+ 
+        const newExpense = new Expense({
+            userId,
+            icon,
+            category,
+            amount,
+            date:new Date(date)
+        });
+
+        await newExpense.save();
+        res.status(200).json(newExpense);
+    }catch(error){
+        res.status(500).json({ message: "Server Error"});
+    }
+
+};
+
+// Get All Expense Source
+exports.getAllExpense = async (req, res) => {
+    const userId = req.user.id;
+    try{
+        const expense = await Expense.find({ userId }).sort({ date: -1 });
+        res.json(expense);
+    }catch (error){
+        res.status(500).json({ message: "Server Error"});
+    }
+};
+
+// Delete Expense Source
+exports.deleteExpense = async (req, res) => {
+    try {
+        await Expense.findByIdAndDelete(req.params.id);
+        res.json({ message: "Expense deleted successfully" });
+    }catch (error) {
+        res.status(500).json({ message: "Server Error"});
+    }
+};
+
+// Download Excel
+// exports.downloadIncomeExcel = async (req, res) => {};
